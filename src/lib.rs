f63719d695d75b21d77f10be876b649b60fd38f2
TucksonDev <tucksondev@gmail.com>
2024-09-03 18:01:10 +0100

    Revert last PR

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
-// Only run this as a WASM if the export-abi feature is not set.
-#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
+#![no_main]
 extern crate alloc;
 
 use alloc::vec::Vec;
-use stylus_sdk::prelude::*;
+use stylus_sdk::stylus_proc::entrypoint;
 
-
-#[storage]
-#[entrypoint]pub struct HelloWorldMinimal {
-}
-
-#[public]
-impl HelloWorldMinimal {
+#[entrypoint]
 fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
     Ok(input)
 }
-}
-
