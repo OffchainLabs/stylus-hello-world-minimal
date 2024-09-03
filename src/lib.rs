94804640e867f3ed817beef4f46d57bf53027714
Mahsa Moosavi <mahsa.msv1991@gmail.com>
2024-09-03 12:10:44 -0400

    cargo stylus export-abi fails

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-#![no_main]
+// Only run this as a WASM if the export-abi feature is not set.
+#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
 extern crate alloc;
 
+
 use alloc::vec::Vec;
-use stylus_sdk::stylus_proc::entrypoint;
+use stylus_sdk::prelude::entrypoint;
 
 #[entrypoint]
 fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
