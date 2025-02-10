6c4588d60205cebf2c9ca7efcc51b8e295b4fc18
Raul Jordan <raul@prysmaticlabs.com>
2025-02-10 15:08:01 -0600

    update version

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+use stylus_sdk::prelude::*;
 use stylus_sdk::stylus_proc::entrypoint;
 
 #[entrypoint]
-fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
+fn user_main(input: Vec<u8>, _vm: stylus_sdk::host::VM) -> Result<Vec<u8>, Vec<u8>> {
     Ok(input)
 }
