0b2114b4d3534ba7777b9a7c878a630b57b8a119
Raul Jordan <raul@prysmaticlabs.com>
2025-02-12 13:07:31 -0600

    edits

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
-#![allow(unused_imports, dead_code)]
 #![no_main]
 extern crate alloc;
 
 use alloc::vec::Vec;
+#[allow(unused_imports)]
 use stylus_sdk::prelude::*;
 use stylus_sdk::stylus_proc::entrypoint;
 
+#[allow(dead_code)]
 #[entrypoint]
 fn user_main(input: Vec<u8>, _vm: stylus_sdk::host::VM) -> Result<Vec<u8>, Vec<u8>> {
     Ok(input)
